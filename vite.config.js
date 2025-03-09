4febec67f57a00f67d51a659d6052fc97cf068a3
Jared <jrubram179@g.educaand.es>
2025-03-09 22:48:39 +0100

    codigo acabado y para entregar

---
+++ vite.config.js
@@ -0,0 +1,34 @@
+import { defineConfig } from 'vite';
+import { resolve } from 'path';
+import { createHtmlPlugin } from 'vite-plugin-html';
+
+export default defineConfig({
+  appType: 'mpa', // Indica que es un proyecto multipágina
+  base: './',
+  root: resolve(__dirname, 'fuente'), // Especifica que "fuente" es la raíz
+  build: {
+    outDir: '../produccion',
+    emptyOutDir: true, // Vacía el directorio de producción antes de compilar
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'fuente/index.html'),
+        contacto: resolve(__dirname, 'fuente/html/contacto.html'),
+        ofertas: resolve(__dirname, 'fuente/html/ofertas.html'),
+        sobreNosotros: resolve(__dirname, 'fuente/html/sobre_nosotros.html')
+      },
+    },
+    minify: 'terser', // Asegura que JS y CSS también se minimicen
+  },
+  plugins: [
+    createHtmlPlugin({
+      minify: true, // Minimiza el HTML
+    }),
+  ],
+  server: {
+    open: true,
+  },
+  preview: {
+    open: true, // Abre automáticamente la previsualización
+    port: 4173, // Puerto para el servidor de previsualización
+  },
+});
